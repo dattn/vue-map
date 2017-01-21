445b55a17048d9c0a1ecce966eccf2bbd8d4b0c2
Daniel Duton <daniel@duton.lu>
2017-01-21 11:24:56 +0100

    check if container has addLayer function

--- src/components/mixins/LayerContainer.js
+++ src/components/mixins/LayerContainer.js
@@ -3,6 +3,9 @@ export default {
   beforeCreate () {
     this.$containerReady = new Promise(resolve => {
       this.$setContainer = container => {
+        if (!container.addLayer) {
+          throw new Error('A LayerContainer component needs an addLayer method')
+        }
         this.$container = container
         resolve(container)
       }
